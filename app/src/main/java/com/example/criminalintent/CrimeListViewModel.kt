73cf9e8d516d4ca8f8e49e56d0131eb349f2481c
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-10-08 20:49:07 +0300

    -add RecyclView -adapter -add new style for app

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -7,7 +7,7 @@ class CrimeListViewMode :ViewModel() {
     init {
         for(i in 0 until  100){
             val crime=Crime()
-            crime.title="Crime #$i"
+            crime.title="Crime : #$i"
             crime.isSolved=i%2==0
             crime.Rpolice=if(i%2!=0){
                 true
